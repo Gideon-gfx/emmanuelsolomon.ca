9045e463093b711a8c90983c2c69a8a4c8e03a58
Solomon Gideon <solomongideon251@gmail.com>
2026-01-30 15:42:11 +0100

    Add build script for Hostinger deployment

--- Emmanuelsolomon.ca/public/scores.js
+++
@@ -1,7 +0,0 @@
-function toggleMenu() {
-document.getElementById("navLinks").classList.toggle("active");
-
-// Toggle the Hamburger to X
-const btn = document.getElementById('hamburgerBtn');
-btn.classList.toggle('active');
-}
